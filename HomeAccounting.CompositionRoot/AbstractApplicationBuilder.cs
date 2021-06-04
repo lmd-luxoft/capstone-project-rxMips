9d28f7efdf4d07d4bf70a4a0b79127b8400483eb
Denis Bubnov <denis.bubnov@gmail.com>
2021-06-04 19:31:31 +0400

    add AbstractApplicationBuilder

---
+++ HomeAccounting.CompositionRoot/AbstractApplicationBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeAccounting.CompositionRoot
+{
+    public abstract class AbstractApplicationBuilder
+    {
+        protected readonly IServiceCollection _services;
+
+        protected AbstractApplicationBuilder(IServiceCollection services)
+        {
+            _services = services;
+        }
+        
+        protected abstract void RegisterBusinessLogic();
+        protected abstract void RegisterDataSources();
+        protected abstract void RegisterInfrastructure();
+
+        public void Build()
+        {
+            RegisterInfrastructure();
+            RegisterDataSources();
+            RegisterBusinessLogic();
+        }
+    }
+}
