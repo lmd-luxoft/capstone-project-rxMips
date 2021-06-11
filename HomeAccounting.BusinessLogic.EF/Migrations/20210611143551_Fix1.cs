befcbd8e2bebf22a92d244e4ff0b1aa4739cf42a
Denis Bubnov <denis.bubnov@gmail.com>
2021-06-11 18:37:00 +0400

    fix database

---
+++ HomeAccounting.BusinessLogic.EF/Migrations/20210611143551_Fix1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HomeAccounting.DataSource.EF.Migrations
+{
+    public partial class Fix1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
